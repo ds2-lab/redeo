b2e6c997d8f912f65b250693ab0a3cb952c36493
Ao Wang <awang24@gmu.edu>
2019-06-28 16:38:09 -0400

    finish set

--- server.go
+++ server.go
@@ -194,32 +194,6 @@ func (srv *Server) perform(c *Client, name string) (err error) {
 	return
 }
 
-//func (srv *Server) myPeekCmd(c *Client, channel chan string) error {
-//	for more := true; more; more = c.rd.Buffered() != 0 {
-//		fmt.Println("Peeking cmd")
-//		name, err := c.rd.PeekCmd()
-//		if err != nil {
-//			_ = c.rd.SkipCmd()
-//			fmt.Println("peeking err is ", err)
-//		}
-//		norm := strings.ToLower(name)
-//		// register call
-//		srv.info.command(c.id, norm)
-//
-//		if c.cmd, err = c.readCmd(c.cmd); err != nil {
-//			fmt.Println("read cmd err", err)
-//		}
-//
-//		fmt.Println("cmd name is ", name)
-//		channel <- name
-//		// flush when buffer is large enough
-//		if n := c.wr.Buffered(); n > resp.MaxBufferSize/2 {
-//			err = c.wr.Flush()
-//		}
-//	}
-//	return nil
-//}
-
 // new serve with channel initial，creating a
 // new service goroutine for each.
 func (srv *Server) MyServe(lis net.Listener, cMap map[int]chan interface{}, mappingTable *hashmap.HashMap) error {
@@ -248,49 +222,6 @@ func (srv *Server) MyServe(lis net.Listener, cMap map[int]chan interface{}, mapp
 		id = id + 1
 	}
 }
-
-//event handler
-//func (srv *Server) myServeClient(c *Client, clientChannel chan interface{}, id int, lambdaChannel chan Req) {
-//	fmt.Println("channel id is ", id)
-//	cmdChannel := make(chan string, 1)
-//
-//	// Release client on exit
-//	defer c.release()
-//
-//	// Register client
-//	srv.info.register(c)
-//	defer srv.info.deregister(c.id)
-//
-//	// Init request/response loop
-//	for !c.closed {
-//		// set deadline
-//		if d := srv.config.Timeout; d > 0 {
-//			c.cn.SetDeadline(time.Now().Add(d))
-//		}
-//		go srv.myPeekCmd(c, cmdChannel)
-//
-//		//go func() {
-//		select {
-//		case cmd := <-cmdChannel:
-//			// construct new request
-//			newReq := Req{cmd, c.cmd, id}
-//			//fmt.Println("command from server:", c.cmd)
-//			fmt.Println("newReq is ", newReq)
-//			// send new request to lambda channel
-//			lambdaChannel <- newReq
-//		case b := <-clientChannel:
-//			fmt.Println("final response is ", b)
-//			c.wr.AppendInt(1)
-//			// flush buffer, return on errors
-//			if err := c.wr.Flush(); err != nil {
-//				fmt.Println("flush err2 is ", err)
-//				return
-//			}
-//		}
-//		//}()
-//
-//	}
-//}
 
 // client handler
 func (srv *Server) myServeClient(c *Client, clientChannel chan interface{}, id int, mappingTable *hashmap.HashMap) {
@@ -348,18 +279,18 @@ func (srv *Server) myServeClient(c *Client, clientChannel chan interface{}, id i
 			}
 			ok, err := enc.Verify(shards)
 			fmt.Println("encode status is", ok)
+			// send every shard to the every lambda instance in group
 			group, ok := mappingTable.Get(0)
 			if ok == false {
 				fmt.Println("get lambda instance failed")
 			}
 			for i, shard := range shards {
-				fmt.Println("the ", i, "th shard is ", shard)
 				newReq := SetReq{cmd, key, shard, id}
+				// send new request to lambda channel
 				group.(Group).Arr[i].C <- newReq
+				fmt.Println("the ", i, "th shard is ", shard, "set to lambda complete")
 			}
-			mappingTable.Set(key, group)
-			// send new request to lambda channel
-			//lambdaChannel <- newReq
+			//mappingTable.Set(key, group)
 		case result := <-clientChannel: /*blocking on receive final result from lambda store*/
 			c.wr.AppendBulkString(result.(string))
 			//fmt.Println("final response is ", result)
